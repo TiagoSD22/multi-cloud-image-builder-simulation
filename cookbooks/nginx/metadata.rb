fd7bdb20e8c2d6575512a3464405d308cd78be90
Tiago Siqueira Dionizio <tiagosdionizio@gmail.com>
2025-08-04 22:33:46 -0300

    Add nginx cookbook for automated server provisioning
    Introduces a new Chef cookbook to install and configure nginx on Ubuntu. Includes metadata, a default recipe for package installation, service management, firewall configuration, and health check endpoint. Adds nginx site and index templates tailored for multi-cloud deployment.

---
+++ cookbooks/nginx/metadata.rb
@@ -0,0 +1,12 @@
+name 'nginx'
+maintainer 'DevOps Team'
+maintainer_email 'devops@example.com'
+license 'All Rights Reserved'
+description 'Installs and configures nginx'
+long_description 'Installs and configures nginx web server'
+version '0.1.0'
+chef_version '>= 16.0'
+
+supports 'ubuntu'
+
+depends 'apt'
